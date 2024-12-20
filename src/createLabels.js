8693abb6ec69065c17dc2e8a97d378a600be8392
Matt Dowdell <mdowdell244@gmail.com>
2024-12-20 09:10:23 +0000

    Reorganise into separate scripts

---
+++ src/createLabels.js
@@ -0,0 +1,44 @@
+module.exports = async ({context, core, github}) => {
+	const resp = await github.rest.issues.listLabelsForRepo(context.repo);
+	const have = new Set(resp.data.map(l => l.name));
+
+	const want = [
+		{
+			name: core.getInput('xs-label');
+			description: 'Pull requests with a very small number of lines changed.',
+		},
+		{
+			name: core.getInput('s-label'),
+			description: 'Pull requests with a small number of lines changed.',
+		},
+		{
+			name: core.getInput('m-label'),
+			description: 'Pull requests with a medium number of lines changed.',
+		},
+		{
+			name: core.getInput('l-label'),
+			description: 'Pull requests with a large number of lines changed.',
+		},
+		{
+			name: core.getInput('xl-label'),
+			description: 'Pull requests with a very large number of lines changed.',
+		},
+		{
+			name: core.getInput('xxl-label'),
+			description: 'Pull requests with a very, very large number of lines changed.',
+		},
+	];
+
+	const missing = want.filter(l => !have.has(l.name));
+
+	for (label of missing) {
+		core.debug(`creating label: ${label.name}`);
+
+		await github.rest.issues.createLabel({
+			...context.repo,
+			name: label.name,
+			color: '4f348b',
+			description: label.description,
+		});
+	}
+}
