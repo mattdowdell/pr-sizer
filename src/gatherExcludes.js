0ba34ac71d1bbe665c8861f7adaf5209b2b6b1d0
Matt Dowdell <mdowdell244@gmail.com>
2024-12-20 09:22:38 +0000

    more

---
+++ src/gatherExcludes.js
@@ -0,0 +1,34 @@
+module.exports = async ({context, core, github}) => {
+	const baseRef = process.env.GITHUB_BASE_REF
+
+	let names = '';
+	await exec.exec(
+		'git',
+		['diff', `origin/${baseRef}`, 'HEAD', '--name-only', '--no-renames'],
+		{
+			listeners: {
+				stdout: (data) => {
+					names += data.toString();
+				},
+			}
+		},
+	);
+
+	const files = names.split(/\r?\n/).filter(n => n.length > 0);
+
+	let attrs = '';
+	await exec.exec(
+		'git',
+		['check-attr', 'linguist-generated', 'linguist-vendored', '--', ...files],
+		{
+			listeners: {
+				stdout: (data) => {
+					attrs += data.toString();
+				},
+			}
+		},
+	);
+
+	let excludes = new Set(attrs.split(/\r?\n/).filter(e => e.endsWith(': set')).map(e => e.split(':')[0]));
+	core.setOutput('excludes', [...excludes].join(' '));
+}
